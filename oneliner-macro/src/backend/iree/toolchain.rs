ed672c28bf1301844f3bb3e3309a2a348a623f8a
Zhaolan Huang <zhaolan94@gmail.com>
2026-07-29 17:03:02 +0200

    feat(toolchain): hacky method to fetch target triple from rustc progress args

--- oneliner-macro/src/backend/iree/toolchain.rs
+++ oneliner-macro/src/backend/iree/toolchain.rs
@@ -1,6 +1,8 @@
-use std::ffi::OsString;
+use std::ffi::{OsStr, OsString};
 use std::path::{Path, PathBuf};
 use std::process::{Command, Output};
+
+use std::env;
 
 use proc_macro2::Span;
 
@@ -21,6 +23,46 @@ pub(super) fn run_onnx_converter(input: &Path, output: &Path) -> syn::Result<()>
     run_command(&mut command, "iree-import-onnx")
 }
 
+//TODO: hacky, shall be improved.
+fn target_from_process_args() -> Option<OsString> {
+    let mut args = env::args_os();
+
+    while let Some(arg) = args.next() {
+        if arg == OsStr::new("--target") {
+            return args.next();
+        }
+
+        if let Some(arg) = arg.to_str() {
+            if let Some(target) = arg.strip_prefix("--target=") {
+                return Some(target.into());
+            }
+        }
+    }
+
+    None
+}
+
+//TODO: hacky, shall be improved.
+fn query_rustc_host() -> Option<String> {
+    let compiler = env::current_exe().ok()?;
+
+    let output = Command::new(compiler)
+        .arg("-vV")
+        .output()
+        .ok()?;
+
+    if !output.status.success() {
+        return None;
+    }
+
+    let stdout = String::from_utf8(output.stdout).ok()?;
+
+    stdout
+        .lines()
+        .find_map(|line| line.strip_prefix("host: "))
+        .map(str::to_owned)
+}
+
 pub(super) fn run_iree_compile(
     compile_input: &Path,
     vmfb: &Path,
@@ -29,7 +71,21 @@ pub(super) fn run_iree_compile(
 ) -> syn::Result<()> {
     let compiler = env_first_os(&["ONELINER_IREE_COMPILE", "IREE_COMPILE"])
         .unwrap_or_else(|| OsString::from("iree-compile"));
-    let rust_target = required_env(&["CARGO_BUILD_TARGET", "TARGET"], "Rust target triple")?;
+
+    //TODO: hacky, shall be improved.
+    let rust_target = match required_env(&["CARGO_BUILD_TARGET", "TARGET"], "Rust target triple") {
+        Ok(t) => t,
+        Err(_) => {
+            target_from_process_args().unwrap_or(query_rustc_host().unwrap().into())
+            .into_string().map_err(|_| {
+                    syn::Error::new(
+                        proc_macro2::Span::call_site(),
+                        "target argument is not valid UTF-8",
+                    )
+                })?
+        }
+        
+    };
     let target_info = llvm_target_info_from_rust_triple(&rust_target).map_err(|error| {
         syn::Error::new(
             Span::call_site(),
