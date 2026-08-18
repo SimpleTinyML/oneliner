4f85856916220f7ad84d46425842176e86f341d5
Zhaolan Huang <zhaolan94@gmail.com>
2026-08-18 15:02:46 +0200

    refactor(runtime): make iree-runtime a must.

--- oneliner/build.rs
+++ oneliner/build.rs
@@ -1,7 +1,4 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-
-    if std::env::var_os("CARGO_FEATURE_IREE_RUNTIME").is_some() {
-        println!("cargo:rustc-link-search=native=/");
-    }
+    println!("cargo:rustc-link-search=native=/");
 }
